ea3525216519bdf4c3273f9c475b006a66a044f4
Petr Fila <pet.fila@gmail.com>
2017-12-06 16:54:01 +1100

    completely running backend API

--- api/model/movie.js
+++ api/model/movie.js
@@ -1,16 +1,28 @@
-const mongoose = require ('mongoose');
-mongoose.Promise = Promise;
-mongoose.connect('mongodb://localhost/movies'); //default port
+const mongoose = require('./base');
+const Schema = mongoose.Schema;
+const ObjectID = Schema.Types.ObjectId;
 
-const db = mongoose.connection;
 
-const movieSchema = mongoose.Schema({
+const CommentSchema = Schema({
+  body: String
+})
+
+const person = { type: ObjectID, ref: 'Person' };
+
+const MovieSchema = Schema({
   title: String,
   yearRelease: Number,
-  star: String
+  star: String,
+  comments: [ CommentSchema ],
+  director: { type: ObjectID, ref: 'Person' },
+  cast: [ { actor: person, character: person } ],
+  crew: [ { person: person, role: String } ]
+  // director: { type: ObjectID, ref: 'Person' },
+  // cast: [ { actor: { type: ObjectID, ref: 'Person' }, character: String } ],
+  // crew: [ { person: { type: ObjectID, ref: 'Person' }, role: String } ]
 });
 
 
-const Movie = mongoose.model('Movie', movieSchema);
+const Movie = mongoose.model('Movie', MovieSchema);
 
 module.exports = Movie;
--- api/routes/movies.js
+++ api/routes/movies.js
@@ -1,57 +1,31 @@
 const express = require('express');
 const Movie = require('../model/movie.js')
+const Person = require('../model/person');
 
 const router = express.Router()
+//const render = (movies) => {res.json({movies}) };
 
 router.get('/', (req, res) => {
-  Movie.find().then((movies) => {
-    res.json({movies});
-  });
+  Movie.find()
+    .populate('director')
+    .populate('crew.person')
+    .populate('cast.actor')
+    .populate('cast.character')
+    .then(movies => {
+      console.log("movies are: ", movies);
+      res.json(movies);
+    })
+    .catch(error => res.json({error}))
+
 });
 
+
 router.post('/', (req, res) => {
-  // console.log("req: ", req);
-  console.log("req.body: ", req.body);
-  console.log("req.query: ", req.query);
-  console.log("req.params: ", req.params);
-  console.log("req.url: ", req.url);
-
-
-  // console.log("req: ", req);
-
-  var newMovie = new Movie(req.body);
-
-  // add validation to check that title exists otherwise don't validate
-  // and return status code indicating client side form error
-
-  newMovie.save(function (err, movie) {
-    if (err) {
-      console.log("error is: ", err);
-      res.status(500).json({ error: `Sorry couldn't save movie '${movie.title}'` })
-    }
-
-    res.status(201).json({ movie });
-
-    console.log("it saved movie: ", movie);
-
-    // Movie.findOne(req.body, 'title yearReleased star', function (err, movies) {
-    //   console.log("movies found are: ", movies);
-    //   console.log("error is: ", err); //return handleError(err);
-    //   if (typeof movies !== null) {
-    //     console.log('movie with title %s released in year %s has stars %s.', movies.title, movie.yearReleased, movies.star)
-    //   }
-    // })
-  })
-
-
-  //req.body should be in json because of parse-body middleware
-  // Movie.save().then((movies) => {
-  //   res.json({movies});
-  // json = ...;
-  //Create new movies
-  //then return movie as json
-  //with the correct http response code
-  // res.json({});
+  Movie.create(req.body)
+    .then((movie) => {
+      res.status(201).json(movie).end();
+    })
+    .catch(error => res.json({ error }))
 });
 
 module.exports = router;
